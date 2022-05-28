297c0460fe5883ab45be1549d6c13ee49473ac31
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-05-28 17:39:04 +0200

    array plate in config file

---
+++ config/plates.php
@@ -0,0 +1,196 @@
+<?php
+
+return [
+    [
+        "name" => "CheeseBurger",
+        "image" => ""
+    ],
+    [
+        "name" => "Pizza Margherita",
+        "image" => ""
+    ],
+    [
+        "name" => "Krubby Patty",
+        "image" => ""
+    ],
+    [
+        "name" => "Ravioli di Medusa",
+        "image" => ""
+    ],
+    [
+        "name" => "Gran Crispy",
+        "image" => ""
+    ],
+    [
+        "name" => "Double Chicken",
+        "image" => ""
+    ],
+    [
+        "name" => "Tagliata di manzo",
+        "image" => ""
+    ],
+    [
+        "name" => "Bistecca Fiorentina",
+        "image" => ""
+    ],
+    [
+        "name" => "Carbonara",
+        "image" => ""
+    ],
+    [
+        "name" => "Risotto alla Milanese",
+        "image" => ""
+    ],
+    [
+        "name" => "Amatriciana",
+        "image" => ""
+    ],
+    [
+        "name" => "Sushi",
+        "image" => ""
+    ],
+    [
+        "name" => "Cous Cous",
+        "image" => ""
+    ],
+    [
+        "name" => "Napoli",
+        "image" => ""
+    ],
+    [
+        "name" => "Capricciosa",
+        "image" => ""
+    ],
+    [
+        "name" => "Diavola",
+        "image" => ""
+    ],
+    [
+        "name" => "Patate",
+        "image" => ""
+    ],
+    [
+        "name" => "Boscaiola",
+        "image" => ""
+    ],
+    [
+        "name" => "Pasta aglio olio peperoncino",
+        "image" => ""
+    ],
+    [
+        "name" => "Gricia",
+        "image" => ""
+    ],
+    [
+        "name" => "Pasta alla puttanesca",
+        "image" => ""
+    ],
+    [
+        "name" => "Orecchiette cime di rapa",
+        "image" => ""
+    ],
+    [
+        "name" => "Pasta al pesto",
+        "image" => ""
+    ],
+    [
+        "name" => "Pasta al ragu",
+        "image" => ""
+    ],
+    [
+        "name" => "Involtini thai",
+        "image" => ""
+    ],
+    [
+        "name" => "Involtini di gamberi",
+        "image" => ""
+    ],
+    [
+        "name" => "Ravioli alla griglia",
+        "image" => ""
+    ],
+    [
+        "name" => "Ravioli di manzo",
+        "image" => ""
+    ],
+    [
+        "name" => "Sashimi misto",
+        "image" => ""
+    ],
+    [
+        "name" => "Nigiri maguro",
+        "image" => ""
+    ],
+    [
+        "name" => "Uramaky spicy tonno",
+        "image" => ""
+    ],
+    [
+        "name" => "Tartare salmone",
+        "image" => ""
+    ],
+    [
+        "name" => "Tartare tonno",
+        "image" => ""
+    ],
+    [
+        "name" => "Hamburger bacon",
+        "image" => ""
+    ],
+    [
+        "name" => "Hamburger classico",
+        "image" => ""
+    ],
+    [
+        "name" => "Polpette al sugo",
+        "image" => ""
+    ],
+    [
+        "name" => "Vitello tonnato",
+        "image" => ""
+    ],
+    [
+        "name" => "Scaloppina al limone",
+        "image" => ""
+    ],
+    [
+        "name" => "Pollo al limone",
+        "image" => ""
+    ],
+    [
+        "name" => "BBQ wings",
+        "image" => ""
+    ],
+    [
+        "name" => "Tiramisu",
+        "image" => ""
+    ],
+    [
+        "name" => "Gelato cioccolato",
+        "image" => ""
+    ],
+    [
+        "name" => "Creme caramel",
+        "image" => ""
+    ],
+    [
+        "name" => "Panna cotta",
+        "image" => ""
+    ],
+    [
+        "name" => "Cheesecake",
+        "image" => ""
+    ],
+    [
+        "name" => "Sbriciolona",
+        "image" => ""
+    ],
+    [
+        "name" => "Kebab",
+        "image" => ""
+    ],
+    [
+        "name" => "Falafel",
+        "image" => ""
+    ],
+];

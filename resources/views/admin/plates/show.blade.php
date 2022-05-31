12a23ec0e8217ee3e773d1f902a9b3d54239ace3
AlfaProgrammer <alexloghin@outlook.it>
2022-05-31 15:33:04 +0200

    Validazione dati registrazione e conferma prima di eliminare un piatto

--- resources/views/admin/plates/show.blade.php
+++ resources/views/admin/plates/show.blade.php
@@ -23,16 +23,45 @@
         <span class="text-danger">Non Disponibile</span>
         @endif
         
+
         <div class="d-flex align-items-center">
+            {{-- Form di eliminazione  --}}
             <form action="{{route('admin.plates.destroy', $plate)}}" method="post">
                 @csrf
                 @method('delete')
-                <button type="submit" class="btn btn-danger mr-2">
+
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-danger mr-2" data-toggle="modal" data-target="#confirmPopUp">
                     Elimina
                 </button>
+                
+                <!-- Modal -->
+                <div class="modal fade" id="confirmPopUp" tabindex="-1" role="dialog" aria-labelledby="confirmPopUp" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h5 class="modal-title" id="confirmPopUp">Conferma eiliminazione</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        </div>
+                        <div class="modal-body">
+                            Sei sicuro di volere eliminare questo piatto? 
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
+                            <button type="submit" class="btn btn-primary">Elimina</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+               
             </form>
             
+            
+
             <a type="button" href="{{ route('admin.plates.edit',$plate) }}" class="btn btn-warning my-3">Modifica</a>
+
         </div>
         
     </div>

d344bb20534662ecf2fc8c86b21fd4f5768ccd63
albicorra <alberto2001corrado@gmail.com>
2022-05-31 15:35:25 +0200

    category collegata

--- resources/views/admin/noRestaurant/index.blade.php
+++ resources/views/admin/noRestaurant/index.blade.php
@@ -6,6 +6,9 @@
             <h2>DashBoard</h2>
             <p>Questa è la tua dashboard</p>
             <h3><a href="{{route('admin.plates.index')}}">{{ $restaurant->user->company_name}}</a></h3>
+            @foreach ($restaurant->categories as $category)
+            <span class="badge rounded-pill bg-success mb-2">{{$category->name}}</span>
+            @endforeach
             <p class="description">
                 {{$restaurant->info ? $restaurant->info : 'Nessuna descrizione'}}
             </p>

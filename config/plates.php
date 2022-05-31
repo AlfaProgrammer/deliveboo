4d1ce31db078828e92b119bebc9dfa5ca5c2e68b
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-05-31 12:35:39 +0200

    aggiunta img array plate

--- config/plates.php
+++ config/plates.php
@@ -3,119 +3,119 @@
 return [
     [
         "name" => "CheeseBurger",
-        "image" => ""
+        "image" => "https://rs-menus-api.roocdn.com/images/623f10b1-a601-4598-9ea0-5685d3a80fc5/image.jpeg?width=560&height=315&auto=webp&format=jpg&fit=crop"
     ],
     [
         "name" => "Pizza Margherita",
-        "image" => ""
+        "image" => "https://rs-menus-api.roocdn.com/images/cd16e0d8-6083-4083-86cd-70032053c610/image.jpeg?width=533&height=300&auto=webp&format=jpg&fit=crop&v=1637754420"
     ],
     [
         "name" => "Krubby Patty",
-        "image" => ""
+        "image" => "https://www.innaturale.com/wp-content/uploads/2017/10/krabby-patty.jpg"
     ],
     [
         "name" => "Ravioli di Medusa",
-        "image" => ""
+        "image" => "https://corrieredelvino.it/wp-content/uploads/2019/10/Dim-Sum-Firenze.-Ravioli-di-Manzo-Lardellati.-Foto-di-Giorgio-Dracopulos-1280x720.jpg"
     ],
     [
         "name" => "Gran Crispy",
-        "image" => ""
+        "image" => "https://rs-menus-api.roocdn.com/images/a9618a3d-ea13-4f45-945c-936b67fd673d/image.jpeg?width=560&height=315&auto=webp&format=jpg&fit=crop"
     ],
     [
         "name" => "Double Chicken",
-        "image" => ""
+        "image" => "https://rs-menus-api.roocdn.com/images/13d237d3-2524-4473-a045-a76c0dbb34fd/image.jpeg?width=560&height=315&auto=webp&format=jpg&fit=crop"
     ],
     [
         "name" => "Tagliata di manzo",
-        "image" => ""
+        "image" => "https://static.cookist.it/wp-content/uploads/sites/21/2018/09/tagliata-di-manzo-segreti.jpg"
     ],
     [
         "name" => "Bistecca Fiorentina",
-        "image" => ""
+        "image" => "https://www.braciamiancora.com/wp-content/uploads/2015/08/Bistecca-Fiorentina-Boccanegra-Firenze-5.jpg"
     ],
     [
         "name" => "Carbonara",
-        "image" => ""
+        "image" => "https://www.cookergirl.it/wp-content/uploads/2021/04/IMG_4298-780x1040.jpg"
     ],
     [
         "name" => "Risotto alla Milanese",
-        "image" => ""
+        "image" => "https://www.buttalapasta.it/wp-content/uploads/2011/08/risotto-alla-milanese.jpg"
     ],
     [
         "name" => "Amatriciana",
-        "image" => ""
+        "image" => "https://www.giallozafferano.it/images/219-21923/Spaghetti-all-Amatriciana_650x433_wm.jpg"
     ],
     [
         "name" => "Sushi",
-        "image" => ""
+        "image" => "https://staticfanpage.akamaized.net/wp-content/uploads/sites/21/2020/11/tipi-di-sushi-1.jpg"
     ],
     [
         "name" => "Cous Cous",
-        "image" => ""
+        "image" => "https://www.cucinaperte.it/wp-content/uploads/2021/06/cous-cous-di-verdure.jpg"
     ],
     [
         "name" => "Napoli",
-        "image" => ""
+        "image" => "https://www.donnamoderna.com/content/uploads/2021/08/pizza-napoli.jpg"
     ],
     [
         "name" => "Capricciosa",
-        "image" => ""
+        "image" => "https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-capricciosa.jpg"
     ],
     [
         "name" => "Diavola",
-        "image" => ""
+        "image" => "https://wips.plug.it/cips/buonissimo.org/cms/2012/05/69630013_m.jpg"
     ],
     [
         "name" => "Patate",
-        "image" => ""
+        "image" => "https://www.ricettedalmondo.it/images/foto-ricette/p/29493-pizza-con-patate-e-rosmarino.jpg"
     ],
     [
         "name" => "Boscaiola",
-        "image" => ""
+        "image" => "https://d2j6dbq0eux0bg.cloudfront.net/images/47695117/2129452902.jpg"
     ],
     [
         "name" => "Pasta aglio olio peperoncino",
-        "image" => ""
+        "image" => "https://wips.plug.it/cips/buonissimo.org/cms/2013/04/spaghetti-aglio-olio-e-peperoncino.jpg?w=712&a=c&h=406"
     ],
     [
         "name" => "Gricia",
-        "image" => ""
+        "image" => "https://blog.giallozafferano.it/dulcisinforno/wp-content/uploads/2022/01/Pasta-alla-gricia-4870.jpg"
     ],
     [
         "name" => "Pasta alla puttanesca",
-        "image" => ""
+        "image" => "https://www.giallozafferano.it/images/239-23911/Spaghetti-alla-puttanesca_650x433_wm.jpg"
     ],
     [
         "name" => "Orecchiette cime di rapa",
-        "image" => ""
+        "image" => "https://blog.giallozafferano.it/dulcisinforno/wp-content/uploads/2021/01/Orecchiette-con-cime-di-rapa-3139.jpg"
     ],
     [
         "name" => "Pasta al pesto",
-        "image" => ""
+        "image" => "https://www.oliocarli.it/-/media/ita/hub_editoriale/ricette/immagini-ricette/primi/spaghetti-pesto-b.ashx"
     ],
     [
         "name" => "Pasta al ragu",
-        "image" => ""
+        "image" => "https://blog.giallozafferano.it/zialora/wp-content/uploads/2015/05/DSCN16684.jpg"
     ],
     [
         "name" => "Involtini thai",
-        "image" => ""
+        "image" => "https://primochef.it/wp-content/uploads/2016/04/SH_CUCINA_THAI_ROLLS_INVOLTINI_GAMBERI.jpg"
     ],
     [
         "name" => "Involtini di gamberi",
-        "image" => ""
+        "image" => "https://www.saporedimare.it/wp-content/uploads/elementor/thumbs/involtini_orientali_gamberi-p7srxcw8t8f5db9cl7l37ng5l9roqzchro7lawvmoe.jpg"
     ],
     [
         "name" => "Ravioli alla griglia",
-        "image" => ""
+        "image" => "https://cuchina.it/wp-content/uploads/2020/11/2020-11-07-12-52-48-511-scaled-e1604912676536-585x585.jpg"
     ],
     [
         "name" => "Ravioli di manzo",
-        "image" => ""
+        "image" => "https://cdn.ilclubdellericette.it/wp-content/uploads/2017/06/ravioli-di-carne-al-vapore-790x500.jpg"
     ],
     [
         "name" => "Sashimi misto",
-        "image" => ""
+        "image" => "https://www.gruppogermogli.it/wp-content/uploads/2021/02/sashimi-misto-1.jpg"
     ],
     [
         "name" => "Nigiri maguro",

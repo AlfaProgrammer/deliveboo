331ddd4582b9cb78adbc13b5c1c176bd8e87c1da
albicorra <alberto2001corrado@gmail.com>
2022-06-01 17:59:35 +0200

    layouts index, modifica route show e chiamata api

---
+++ resources/js/components/RestaurantCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card p-5 rounded-lg">
+        <img :src="restaurant.image" alt="">
+        <h3>{{restaurant.name}}</h3>
+        <p>{{restaurant.city}}</p>
+    
+        <div class="categories-wrapper">
+            <span class="badge badge-pill badge-success m"
+            v-for="category in restaurant.categories"
+            :key="category.id">
+    
+                {{category.name}}
+                
+            </span>
+        </div>
+        <router-link :to="{name: 'restaurant.show', params: {slug:restaurant.slug}}" >Esplora</router-link>
+    </div> 
+            
+    
+</template>
+
+<script>
+export default {
+    name: 'RestaurantCard',
+    props: {
+        restaurant: Object,
+        require: true,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/pages/plates/Index.vue
+++ resources/js/pages/plates/Index.vue
@@ -1,32 +1,64 @@
 <template lang="">
-    <main class="container ">
-        
+    <main class="container">
+        <div class="card-wrapper">
+            <RestaurantCard
+                v-for="restaurant in restaurants"
+                :key="restaurant.id"
+                :restaurant="restaurant"    
+            />
+        </div>
+
     </main>
 </template>
 
 <script>
+import RestaurantCard from '../../components/RestaurantCard.vue';
+
 export default {
     data() {
         return {
             restaurants: [],
         }
     },
+    components: {
+        RestaurantCard,
+    },
     methods: {
-        fetchRestaurant() {
+        fetchRestaurants() {
             axios.get('/api/restaurants')
                 .then(res => {
                     const {restaurants} = res.data;
                     this.restaurants = restaurants;
-                    console.log(this.restaurants);
                 })
         }
     },
     beforeMount() {
-        this.fetchRestaurant()
+        this.fetchRestaurants()
     }
 }
 </script>
 
-<style lang="">
-    
+<style lang="scss" scoped>
+    .card-wrapper{
+        display: flex;
+        align-items: center;
+        gap: 30px;
+        justify-content: center;
+        flex-wrap: wrap;
+
+        .card{
+            width: 300px;
+            height: 400px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+
+            .categories-wrapper{
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+        }
+    }
 </style>

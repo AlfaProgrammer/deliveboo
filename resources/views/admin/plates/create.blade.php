a60b47f717208cb09b5fbe333a2756bc0228ba2c
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-06-02 10:43:46 +0200

    aggiunta script in create plate form

--- resources/views/admin/plates/create.blade.php
+++ resources/views/admin/plates/create.blade.php
@@ -6,12 +6,12 @@
 
         <h1>Inserisci un nuovo piatto</h1>
         
-        <form action="{{ route('admin.plates.store') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('admin.plates.store') }}" method="POST" enctype="multipart/form-data" class="needs-validation" novalidate>
             @csrf
             
             <div class="form-group">
-                <label for="name">Nome</label>
-                <input class="form-control form-control-lg @error('name') is-invalid @enderror" type="text" name="name" placeholder="Inserisci il nome del piatto" value="{{old('name')}}">
+                <label for="name">Nome*</label>
+                <input class="form-control form-control-lg @error('name') is-invalid @enderror" type="text" name="name" placeholder="Inserisci il nome del piatto" value="{{old('name')}}" required>
                 @error('name')
                 <div class="invalid-feedback">{{$message}}</div>
                 @enderror
@@ -46,31 +46,55 @@
                 <label for="description">Descrizione</label>
                 <textarea class="form-control" id="FormControlTextarea1" name="description" rows="3" placeholder="Inserisci descrizione piatto">{{old('description')}}</textarea>
                 @error('description')
-                <div class="invalid-feedback">{{$message}}</div>
+                    <div class="invalid-feedback">{{$message}}</div>
                 @enderror
             </div>
 
             <div>
-                <label for="price" class="input-number-label">Prezzo</label>
+                <label for="price" class="input-number-label">Prezzo*</label>
                 <span class="input-number input-number-currency">
-                    <input type="number" id="price" name="price" step="0.50" value="0.00" min="0" max="999.99" />
+                    <input class="form-control @error('price') is-invalid @enderror" type="number" id="price" name="price" step="0.50" placeholder="0.00" min="0" max="999.99" required/>
                     <span class="sr-only">Aumenta valore Euro</span>
                     <span class="sr-only">Diminuisci valore Euro</span>
                 </span>
+                @error('price')
+                    <div class="invalid-feedback">{{$message}}</div>
+                @enderror
             </div>
 
-            <div class="select-wrapper my-3">
-                <label for="available">Disponibile</label>
-                <select id="available" name="available">
-                <option selected="" value="">Scegli un'opzione</option>
-                <option value="1">Disponibile</option>
-                <option value="0">Non disponibile</option>
+            <div class="select-wrapper my-3" required>
+                <label for="available">Disponibile*</label>
+                <select id="available" name="available" class="custom-select">
+                    <option value="">Scegli un'opzione</option>
+                    <option value="1">Disponibile</option>
+                    <option value="0">Non disponibile</option>
                 </select>
             </div>
 
             <button type="submit" class="btn btn-primary my-3">Aggiungi</button>
 
         </form>
+
+        <script>
+            // Example starter JavaScript for disabling form submissions if there are invalid fields
+            (function() {
+              'use strict';
+              window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                  form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                      event.preventDefault();
+                      event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                  }, false);
+                });
+              }, false);
+            })();
+        </script>
         
     </div>
     

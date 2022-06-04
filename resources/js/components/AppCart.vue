5e31fe2efe933cb9491c81c49677b2acbae292dc
AlfaProgrammer <alexloghin@outlook.it>
2022-06-04 22:40:59 +0200

    Store e Modules
    Implementato store.js basico.
    Ho creato anche il modulo cartModule da associare allo store
    Dentro cartModule riesco a fare il fetching dei piatti del ristorante
    grazie al recupero dello slug dalla pagina show tramite prop che viene
    poi passato al method fetchPlates dentro il componente AppCart che nel
    frattempo sta mappando appunto l'azione fetchPlates dentro cartModule
    che tralaltro è namespaced: true

---
+++ resources/js/components/AppCart.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <p>carrello</p>
+      <p v-for="(item, index) in cart" :key="index"> {{ item }} </p>
+      <p>{{slug}}</p>
+      <button @click="fetchRestaurant()"> Cliccami </button>
+  </div>
+</template>
+
+<script>
+import {mapState, mapActions} from 'vuex';
+export default {
+    props:[
+        'slug'  // ho bisogno dello slug da passare alla chiamata in store.js 
+    ],
+    computed: {
+        // fatto minding del path al modulo cartModule per recuperare l'árray cart
+        ...mapState('cartModule', ['cart']),
+
+    },
+    methods:{
+        fetchRestaurant(){
+            this.$store.dispatch({
+                type: 'cartModule/fetchRestaurant',
+                slug: this.slug
+            })
+        }
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/store/modules.js
@@ -0,0 +1,44 @@
+export const cartModule = {
+    namespaced:  true,
+
+    state: {
+        plates: [],
+        cart: ['item1', 'item2', 'item3'],
+        orders: [],
+        // slug: this.$route.params.slug,
+
+
+    },
+    mutations: {
+        cartFill(plate){
+
+        }
+    },
+    actions: {
+        fetchRestaurant( store, payload ) {
+
+            // console.log(payload.slug)
+            
+            axios.get(`/api/restaurants/${payload.slug}`)
+                .then( res => {
+                    console.log(res.data.plates)
+                })
+
+            //         // const { plates } = res.data;
+            //         // this.restaurant = restaurant;
+            //         // this.plates = plates;
+            //         // console.log(this.restaurant);
+            //         // this.loading = true;
+            //         // console.log(res.data);
+            //     })
+                /* .catch(err => {
+                    this.router.push('/404');
+                }) */
+        }
+    },
+
+    getters:{
+        // non li utilizziamo perche useremo la funzione helper mapState per il momento
+        // se dovessimo fare dei calcoli prima di restituire una proprieta in state, utilizzeremo i getter.
+    }
+}

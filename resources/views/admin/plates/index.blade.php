060822b21f0b677458f6f7551b5fe6c7ab3ebf78
M1571 <94639639+M1571@users.noreply.github.com>
2022-06-05 12:08:29 +0200

    index card plates

--- resources/views/admin/plates/index.blade.php
+++ resources/views/admin/plates/index.blade.php
@@ -4,26 +4,68 @@
 @section('content')
     <div class="container" >
         @if ($plates)
-        <h3>{{$user->restaurant->name}}</h3>
-        <img src="{{ $user->restaurant->image }}" alt="">
-            @foreach ($plates as $plate)
-                <div>
-                    <img src="{{ $plate->image ?: '' }}" style="width: 100px">
-                    <a href="{{route('admin.plates.show', $plate)}}">{{$plate['name']}}</a>
-                </div>
+        <div class="d-flex flex-column mb-5">
+            <h3>{{$user->restaurant->name}}</h3>
+            <img src="{{ $user->restaurant->image }}" alt="">
+        </div>
 
-                @foreach ($plate->allergens as $allergen)
+            {{-- @foreach ($plates as $plate)
+                
+                <div class="">
+                    <div>
+                        <img src="{{ $plate->image ?: '' }}" style="width: 100px">
+                        <a href="{{route('admin.plates.show', $plate)}}">{{$plate['name']}}</a>
+                    </div>
+                    
+                    @foreach ($plate->allergens as $allergen)
                     <span class="badge rounded-pill bg-success mb-2">{{$allergen->name}}</span>
-                @endforeach
-
+                    @endforeach
+                    
             @endforeach
 
-            <div>
-                
-                <a href="{{ route('admin.plates.create') }}" class="btn btn-primary">Crea nuovo piatto</a>
-                <a href="{{ route('admin.home') }}" class="btn btn-warning">Torna alla dasboard</a>
+                </div> --}}
 
-            </div>
+                {{-- ----------------------- --}}
+                {{-- CARDS --}}
+
+                <div class="row">
+
+                    <div class="row mb-5">
+
+                    @foreach ($plates as $plate)
+
+                        <div class="card col-3">
+
+                            <img class="card-img-top" src="{{ $plate->image ?: '' }}" style="width: 100%">
+
+                            <div class="card-body d-flex flex-column">
+                                <a href="{{route('admin.plates.show', $plate)}}">{{$plate['name']}}</a>
+                                <div>
+                                    @foreach ($plate->allergens as $allergen)
+                                    <span class="badge rounded-pill bg-success mb-2">{{$allergen->name}}</span>
+                                    @endforeach
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    @endforeach
+
+                    </div>
+
+                </div>
+
+                {{-- CARDS --}}
+                {{-- ----------------------- --}}
+                
+                <div>
+                    
+                    <a href="{{ route('admin.plates.create') }}" class="btn btn-primary">Crea nuovo piatto</a>
+                    <a href="{{ route('admin.home') }}" class="btn btn-warning">Torna alla dasboard</a>
+                    
+                </div>
+
         @endif
     </div>
         

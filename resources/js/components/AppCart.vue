6e00e3e9dc36e4f5ac723cdfd911a1716e814ec5
AlfaProgrammer <alexloghin@outlook.it>
2022-06-08 13:47:48 +0200

    Ogni ristorante ha il proprio carrello

--- resources/js/components/AppCart.vue
+++ resources/js/components/AppCart.vue
@@ -2,17 +2,19 @@
 
   <div>
       <h1 class="font-bold text-xl mb-5">Carrello</h1>
-
       <div>
-          <div v-if="cart.length < 1 "> 
-              <p>Non ci sono articoli nel tuo carrello</p> 
+          <div v-if="restaurant_cart.length < 1 "> 
+              <p>
+                  Non ci sono articoli nel tuo carrello per questo ristorante <br>
+                  Ricordati che puoi acquistare da un ristorante per volta.
+              </p> 
           </div>
           <div v-else>
-                <div class="totalPrice">
+                <!-- <div class="totalPrice">
                     <p>{{formatCurrency(cartTotal)}}</p>
-                </div>
+                </div> -->
 
-                <ul v-for="(plate, index) in cart" :key="index">
+                <ul v-for="(plate, index) in restaurant_cart" :key="index">
                     <li class="flex justify-between items-center p-[20px] shadow-lg border-red">
                         <div class="cart-item-wrapper flex gap-[20px]">
 
@@ -22,7 +24,7 @@
 
                             <div class="item-info">
                                 <h3>{{ plate.name }}</h3>
-                                <p>Tatale Carrello: {{formatCurrency(plate.price)}}</p>
+                                <p>Prezzo: {{formatCurrency(plate.price)}}</p>
                             </div>
 
                         </div>
@@ -36,29 +38,30 @@
                 </ul> 
         </div>
       </div>
-
   </div>
 </template>
 
 <script>
-import {mapState, mapActions} from 'vuex';
+// import { mapState, mapActions } from 'vuex'
 export default {
+    data(){
+        return{
+
+        }
+    },
     props:[
-        // 'slug',  // ho bisogno dello slug da passare alla chiamata in store.js 
-        // 'plates'
+        'restaurant_cart', 
         'formatCurrency'
     ],
-    computed: {
-        // fatto minding del path al modulo cartModule per recuperare l'árray cart
-        ...mapState('cartModule', ['cart', 'cartTotal']),
-
+    computed:{
+        //code...
     },
     methods:{
-        ...mapActions('cartModule', [
-            // 'fillCartFromStorage',
-            'createCartStorage',
-            'updateCart',
-        ]),
+        // ...mapActions('cartModule', [
+        //     // 'fillCartFromStorage',
+        //     'createCartStorage',
+        //     'updateCart',
+        // ]),
         removeFromCart(plate){
             this.$store.dispatch({
                 type: 'cartModule/removeFromCartStorage',
@@ -69,11 +72,11 @@ export default {
     },
 
     beforeMount(){
-        if( ! localStorage.cart ){
-            this.createCartStorage()
-        } else {
-            this.updateCart()
-        }     
+        // if( ! localStorage.cart ){
+        //     this.createCartStorage()
+        // } else {
+        //     this.updateCart()
+        // }     
     }
 }
 </script>

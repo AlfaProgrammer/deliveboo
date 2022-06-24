14fa790507167c8d9cf4ae173a0caa70795fc7ce
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-06-24 11:37:55 +0200

    color

--- resources/js/chart.js
+++ resources/js/chart.js
@@ -7,17 +7,36 @@ const graph = new Chart(myChart, {
     type: 'bar',
     data: {
         labels: [
-            'gennaio', 
-            'febbraio', 
-            'marzo', 
-            'aprile',
-            'maggio',
-            'giugno',
-            'luglio',
+            'Gennaio', 
+            'Febbraio', 
+            'Marzo', 
+            'Aprile',
+            'Maggio',
+            'Giugno',
+            'Luglio',
+            'Agosto',
+            'Settembre',
+            'Ottobre',
+            'Novembre',
+            'Dicembre'
         ],
         datasets: [{
-            labels: 'day',
-            data: [30, 27, 31, 30, 31, 30, 31],
+            label: 'Ordini',
+            data: [30, 27, 31, 30, 31, 30, 31,74,12,45,66,32],
+            backgroundColor: [
+                'rgba(0, 204, 188, 0.6)',
+                'rgba(127, 22, 224, 0.6)',
+                'rgba(0, 224, 206, 0.6)',
+                'rgba(224, 88, 22, 0.6)',
+                'rgba(217, 224, 11, 0.6)',
+                'rgba(68, 0, 99, 0.6)',
+                'rgba(224, 106, 22, 0.6)',
+                'rgba(153, 0, 224, 0.6)',
+                'rgba(175, 224, 22, 0.6)',
+                'rgba(11, 207, 224, 0.6)',
+                'rgba(206, 223, 242, 0.6)',
+                'rgba(12, 247, 124, 0.6)'
+            ]
         }]
     },
     options: {}
